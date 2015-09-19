075613284ce9f4c12e0855d2c1c483737ca41b4d
lijieaa <952054767@qq.com>
2015-09-19 09:25:44 +0800

    增强for

---
+++ src/main/java/Demo1.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Administrator on 2015/9/19.
+ */
+public class Demo1 {
+
+    public static void main(String[] args) {
+        int arr[] = {1,2,3};
+
+        for (int i : arr) {
+            System.out.println(i);
+        }
+    }
+}
