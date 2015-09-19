d786ada708f742ae545fd8d20e5721ad7849ea76
lijieaa <952054767@qq.com>
2015-09-19 09:42:30 +0800

    增强for2

--- src/main/java/Demo1.java
+++ src/main/java/Demo1.java
@@ -1,13 +1,30 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+
 /**
  * Created by Administrator on 2015/9/19.
  */
 public class Demo1 {
 
     public static void main(String[] args) {
+    }
+
+    @Test
+    public void test1() {
         int arr[] = {1,2,3};
 
-        for (int i : arr) {
-            System.out.println(i);
+        for (int i : arr) System.out.println(i);
+    }
+
+    @Test
+    public void test2() throws Exception {
+        ArrayList<Integer> integers = new ArrayList<Integer>();
+        integers.add(4);
+        integers.add(5);
+        integers.add(6);
+        for (Integer integer : integers) {
+            System.out.println(integer);
         }
     }
 }
